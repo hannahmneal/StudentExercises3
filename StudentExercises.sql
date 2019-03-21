34aac8b67375c6ea8115c5059dbee1ce9ab24efd
Hannah Neal <hannahmneal@gmail.com>
2019-03-21 01:17:32 -0500

    added remaining files

--- StudentExercises.sql
+++ StudentExercises.sql
@@ -50,4 +50,67 @@ CREATE TABLE Student (
 	StudentLastName VARCHAR(55) NOT NULL,
 	StudentSlackHandle VARCHAR(55) NOT NULL,
 	student_cohort_id INTEGER NOT NULL,
+
 );
+
+INSERT INTO Cohort (CohortName) VALUES ('Cohort 29');
+INSERT INTO Cohort (CohortName) VALUES ('Cohort 30');
+INSERT INTO Cohort (CohortName) VALUES ('Cohort 31');
+
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Brittany', 'Ramos-Janeway','@bramosa', 1);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Allison', 'Collins', '@allison', 1);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Ashwin', 'Prakash', '@ashwin', 1);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Zac', 'Crawford', '@zac', 1);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Brian', 'Neal', '@bubbabrine', 2 );
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Abbey', 'Brown', '@abbeybrown',2);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Sam', 'Cooper', '@sam', 2);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Meag', 'Mueller', '@meag', 3);
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Joel', 'Mondesir', '@joel', 3 );
+INSERT INTO Student (StudentFirstName, StudentLastName, StudentSlackHandle, student_cohort_id) VALUES ('Dek', 'Haji', '@dek',3);
+
+INSERT INTO Instructor (InstructorFirstName, InstructorLastName, InstructorSlackHandle, instructor_cohort_id) VALUES ('Jisie', 'David', '@jisie', 3);
+INSERT INTO Instructor (InstructorFirstName, InstructorLastName, InstructorSlackHandle, instructor_cohort_id) VALUES ('Andy', 'Collins', '@handyandy', 1);
+INSERT INTO Instructor (InstructorFirstName, InstructorLastName, InstructorSlackHandle, instructor_cohort_id) VALUES ('Steve','Brownlee','@coach', 2);
+INSERT INTO Instructor (InstructorFirstName, InstructorLastName, InstructorSlackHandle, instructor_cohort_id) VALUES ('Lemmily','Emmon','@lemily', 1);
+
+
+INSERT INTO Exercise (ExerciseName, ExerciseLanguage) VALUES ('Welcome to Nashville', 'Javascript'); 
+INSERT INTO Exercise (ExerciseName, ExerciseLanguage) VALUES ('Ternary Traveler', 'Javascript'); 
+INSERT INTO Exercise (ExerciseName, ExerciseLanguage) VALUES ('Nutshell', 'React'); 
+INSERT INTO Exercise (ExerciseName, ExerciseLanguage) VALUES ('Capstone', 'React'); 
+INSERT INTO Exercise (ExerciseName, ExerciseLanguage) VALUES ('DepartmentsAndEmployees', 'C#'); 
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (5, 1);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 1);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (5, 2);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 2);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (5, 3);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 3);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (5, 4);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 4);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (3, 5);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 5);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (3, 6);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 6);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (3, 7);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 7);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (1, 8);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (2, 8);
+
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (1, 9);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (2, 9);
+
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (3, 10);
+INSERT INTO StudentExercises (assigned_exercise_id, student_id) VALUES (4, 10);
+
+SELECT * FROM Cohort;
+SELECT * FROM Exercise;
+SELECT * FROM Student;
